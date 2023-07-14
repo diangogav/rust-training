fd2eb1e40ad2d35148dc53dfd8cd0bd22f604003
Diango Gavidia <diangogavidia@gmail.com>
2023-07-14 12:16:28 -0400

    Solve numbers activity

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -5,7 +5,16 @@
 //
 // Notes:
 // * Use a function to add two numbers together
+fn add(a: i32, b: i32) -> i32 {
+  return a + b;
+}
 // * Use a function to display the result
+fn print_result(result: i32) {
+  println!("Result: {:?}", result);
+}
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+  let result = add(5, 2);
+  print_result(result);
+}
