222979a34a8f4595b30c9e3361a35b2d17015057
Diango Gavidia <diangogavidia@gmail.com>
2023-08-04 22:08:09 -0400

    Tuples activity resolved

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,18 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn coordinate() -> (i32, i32) {
+  return (10, 10);
+}
+
+fn main() {
+  let (_x, y) = coordinate();
+
+  if y > 5 {
+    println!("{:?} greater than 5", y)
+  } else if y == 5 {
+    println!("{:?} equal to 5", y)
+  } else {
+    println!("{:?} less than 5", y)
+  }
+}
